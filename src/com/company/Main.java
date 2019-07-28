d7422aceca34580c4deee556d1e15b2ccbf5d3ec
kasambuli <cynthiakasambuli@gmail.com>
2019-07-28 19:59:13 +0300

    Created the display for the encoded message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,6 @@ package com.company;
 
 import java.io.Console;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 public class Main {
     Console myConsole = System.console();
@@ -10,13 +9,11 @@ public class Main {
         Scanner inputString = new Scanner(System.in);
         System.out.println("Enter your desired string");
         String userStringInput = inputString.nextLine();
-//        String  = "Lawrence Karanja";
         System.out.println("Okay, I will decode" + " " + userStringInput);
         Cipher cipher = new Cipher(userStringInput);
         char[] encoded = cipher.makeUserInputCharacterArray();
         System.out.println("Okay, here is your character array fro the input" + " " + Arrays.toString(encoded));
-        List<Integer> indexOfCharacters = cipher.getAlphabet();
-        System.out.println("Okay, here is your list of indexes" + " " + indexOfCharacters);
-
+        String indexOfCharacters = cipher.getAlphabet();
+        System.out.println("Okay, here is encoded String" + " " + indexOfCharacters);
     }
 }
